3717edcaf4678ae1a35dff4ef612eeea94ca7548
Meg Scholl <meghan.e.scholl@gmail.com>
2018-04-20 15:07:13 -0500

    working on the design and dev pages, and changed the nav's

---
+++ javascript/contact.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let contactPage = document.getElementById("contact--page");
+
+function showContactPage() {
+
+    console.log("showContactPage function coming through");
+}
+showContactPage();
+
+module.export = {showContactPage}
