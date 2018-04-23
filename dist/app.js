7d0c1c5925fea93179198daef06fb83d2ffdfeb7
Meg Scholl <meghan.e.scholl@gmail.com>
2018-04-23 13:02:34 -0500

    app

--- dist/app.js
+++ dist/app.js
@@ -47,17 +47,10 @@ console.log("showDesignDevPage function");
                         <img src="./images/languages/bootstrap.png" width="4%" alt="" title="">
                         <img src="./images/languages/css3.png" width="3.5%" alt="" title="">
                         <img src="./images/languages/html5.png" width="5%" alt="" title="">
-<<<<<<< HEAD
                         <img src="./images/languages/javascript.png" width="4%" alt="" title="">
                         <img src="./images/languages/jquery.png" width="4%" alt="" title="">
                         <img src="./images/languages/materialize.png" width="5%" alt="" title="">
                         <img src="./images/languages/react.png" width="4%" alt="" title="">
-=======
-                        <img src="../images/languages/javascript.png" width="4%" alt="" title="">
-                        <img src="../images/languages/jquery.png" width="4%" alt="" title="">
-                        <img src="../images/languages/materialize.png" width="5%" alt="" title="">
-                        <img src="../images/languages/react.png" width="4%" alt="" title="">
->>>>>>> gh-pages
 
                     </div>  
                     <br>
