8b50afb80da0ca9dce0855c7d1a4852a88e9155c
Meg Scholl <meghan.e.scholl@gmail.com>
2018-05-06 23:54:16 -0500

    moved some images around for better flow...would like to put projects inside cards

--- dist/app.js
+++ dist/app.js
@@ -48,6 +48,14 @@ function showAboutMePage() {
                     Since then, Meg earned her Bachelor of Science Degree from Westfield State University in Environmental Science, Regional Planning, and Geographic Information Systems (GIS). After graduating, she relocated to Nashville with her family, where she was presented with an abudance of opportunities including publishing, engineering, volunteer management and software development. She is currently enrolled in a full-time advanced UX UI Design Front-End Development program at Nashville Software School.
                 </p>
             </div>   
+
+            
+        <div class="programs">
+                 
+                            
+        <img src="./../images/ux_vs_ui.png" alt="ux vs ui diagram" title="ux vs ui diagram" width="100%">
+  
+        </div>
     
         </div>        
 
@@ -228,38 +236,29 @@ console.log("showDesignDevPage function");
 
 
         <main>
-        <div class="introContainer">
-                 
-                            
-                    <img src="./../images/ux_vs_ui.png" alt="ux vs ui diagram" title="ux vs ui diagram" width="100%">
-                    <div class="flex-item">  
-                        <h2>Programs I Use:</h2>
-                            <img src="../images/languages/html5.png" width="10%" alt="HTML5 logo" title="HTML5 logo"/>
-                            <img src="../images/languages/bootstrap.png" width="8%" alt="Bootstrap logo" title="Bootstrap logo"/>
-                            <img src="../images/languages/css3.png" width="7%" alt="CSS3 logo" title="CSS3 logo"/>
-                            <img src="../images/languages/javascript.png" width="8%" alt="JAVASCRIPT LOGO" title="JAVASCRIPT LOGO"/>
-                            <img src="../images/languages/jquery_logo.png" width="8%" alt="JQUERY LOGO" title="JQUERY LOGO"/>
-                            <img src="../images/languages/materialize.png" width="11%" alt="MATERIALIZE LOGO" title="MATERIALIZE LOGO"/>
-                            <img src="../images/languages/react.png" width="8%" alt="REACT LOGO" title="REACT LOGO"/>
-                    </div>  
-        </div>
-
-        <br><br>
-        <br><br>
+            <div class="projects-container">
+            <h2>Projects</h2>
+                <img src="../images/reso_snapshot.PNG" width="700px" alt="RESO" title="RESO" style="float: left"/><br>
+                <div class="project-desc"><p style="border: solid thin black">Reso is a reservation application that I developed for my mid-term capstone project. This product allows a user to use in with Google Authentication through Firebase, create, edit, and delete reservations to their preferred restaurant. Reso is unique in that the main feature is for the user to be able to check-in to their reservation. To learn about the UX research conducted for this project, please click <a href="https://docs.google.com/presentation/d/1gcjwK-m7v9mE-_HfLS7s5eadAqZCvxJckkhoJKZ3P4E/edit?usp=sharing" alt="Google document with UX research for Reso" title="Google document containing UX research for Reso">here</a>. To learn more about the development of Reso, click <a href="https://github.com/megscholl/capstone" class="project-links" alt="RESO" title="RESO"">here</a>.</p></div>
+            </div>
         <br><br>
 
-                    <div class="projects-container">
-                        <img src="../images/reso_snapshot.PNG" width="700px" alt="RESO" title="RESO" style="float: left"/>
-                        <div class="project-desc"><p style="padding-left: 1rem">Reso is a reservation application that I developed for my mid-term capstone project. This product allows a user to use in with Google Authentication through Firebase, create, edit, and delete reservations to their preferred restaurant. Reso is unique in that the main feature is for the user to be able to check-in to their reservation. To learn about the UX research conducted for this project, please click <a href="https://docs.google.com/presentation/d/1gcjwK-m7v9mE-_HfLS7s5eadAqZCvxJckkhoJKZ3P4E/edit?usp=sharing" alt="Google document with UX research for Reso" title="Google document containing UX research for Reso">here</a>. To learn more about the development of Reso, click <a href="https://github.com/megscholl/capstone" class="project-links" alt="RESO" title="RESO"">here</a>.</p></div>
-                    </div>
-        <br><br>
+            <div class="projects-container">
+                <img src="../images/tend_snapshot.PNG" width="680px" alt="Tend Income Tracker; Image credit to HopscotchArt" title="Tend Income Tracker; Image credit to HopscotchArt" style="float: left"/>
+                <div class="project-desc"><p>Tend is a personal project. I've been unable to find a budget or income tracker that I really like, so I decided to make me own. This single-page application is still in development, however I have main and stretch goals in place. You may read more about Tend <a href="https://github.com/megscholl/budget-tracker" class="project-links" alt="Tend Budget Tracker" title="Tend Budget Tracker">here</a>.</p></div>
+            </div>
 
-                    <div class="projects-container">
-                        <img src="../images/tend_snapshot.PNG" width="680px" alt="Tend Income Tracker; Image credit to HopscotchArt" title="Tend Income Tracker; Image credit to HopscotchArt" style="float: left"/>
-                        <div class="project-desc"><p>Tend is a personal project. I've been unable to find a budget or income tracker that I really like, so I decided to make me own. This single-page application is still in development, however I have main and stretch goals in place. You may read more about Tend <a href="https://github.com/megscholl/budget-tracker" class="project-links" alt="Tend Budget Tracker" title="Tend Budget Tracker">here</a>.</p></div>
-                    </div>
-
-            </main>
+            <div class="flex-item">  
+            <h2>Programs I Use:</h2>
+                <img src="../images/languages/html5.png" width="10%" alt="HTML5 logo" title="HTML5 logo"/>
+                <img src="../images/languages/bootstrap.png" width="8%" alt="Bootstrap logo" title="Bootstrap logo"/>
+                <img src="../images/languages/css3.png" width="7%" alt="CSS3 logo" title="CSS3 logo"/>
+                <img src="../images/languages/javascript.png" width="8%" alt="JAVASCRIPT LOGO" title="JAVASCRIPT LOGO"/>
+                <img src="../images/languages/jquery_logo.png" width="8%" alt="JQUERY LOGO" title="JQUERY LOGO"/>
+                <img src="../images/languages/materialize.png" width="11%" alt="MATERIALIZE LOGO" title="MATERIALIZE LOGO"/>
+                <img src="../images/languages/react.png" width="8%" alt="REACT LOGO" title="REACT LOGO"/>
+            </div>
+        </main>
 
 
             <br>
